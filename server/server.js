b6b599489d083d7c8ea17283b0410d087f04e61a
jamesmacalino <hootenmeyer@gmail.com>
2023-12-02 11:53:27 -0500

    update to server.js

--- server/server.js
+++ server/server.js
@@ -8,14 +8,14 @@ const e = require('express');
 
 //CRUD
 console.log("~~~~CRUD~~~~~~")
-console.log(process.env.MONGO_URI);
+console.log(process.env.REACT_APP_MONGO_URI);
 console.log("~~~~~~~~~~")
 
 //path info
 console.log("~~~~dirname~~~~~~~~")
 console.log(__dirname)
 console.log("~~~~PORT~~~~~~~~")
-console.log(process.env.PORT);
+console.log(process.env.REACT_APP_PORT);
 console.log("~~~~~~~~~~~~")
 
 // used to serve static files from public directory
@@ -136,7 +136,7 @@ app.get('/account/all', function (req, res) {
     });
 });
 
-const port = process.env.PORT || 3500;
+const port = process.env.REACT_APP_PORT || 3500;
 console.log('Running on port: ' + port);
 //const PORT = process.env.PORT;
 //app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
